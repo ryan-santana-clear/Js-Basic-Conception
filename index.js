c36f445e87b93ed434c7ee5e552cee6ac528c7c1
Ryan Santana <ryan.santana.clear@gmail.com>
2023-02-14 10:38:17 -0300

    Learning about operatos and practicing

--- index.js
+++ index.js
@@ -8,3 +8,16 @@ console.log(HelloWorld);
 console.log(HelloWorld);
 console.log(HelloWorld);
 console.log(HelloWorld);
+
+/**
+ * Learning  Operators
+ */
+
+let a = 10;
+const b = "10";
+
+console.log(a == b);  // True
+console.log(a === b); // False
+console.log(a !== b); // True
+console.log(a == 10 && b == 10);  // True
+console.log(a == b || typeof a == 'string'); // True becaseu compare type number with string
