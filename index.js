46f33427649a52cca6ee6aaaf6bf35b58ddcad0d
Ryan Santana <ryan.santana.clear@gmail.com>
2023-02-14 08:48:43 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * Learning Basics Concepts of JS
+ */
+var HelloWorld = "Hello World";
+
+//console.log('Hello World!');
+console.log(HelloWorld);
+console.log(HelloWorld);
+console.log(HelloWorld);
+console.log(HelloWorld);
