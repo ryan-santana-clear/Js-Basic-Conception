9718a73bf20e74c40578ee9ce44edebfa3dad6a8
Ryan Santana <ryan.santana.clear@gmail.com>
2023-02-14 13:07:45 -0300

    Learning and creating a class

--- index2.js
+++ index2.js
@@ -26,3 +26,39 @@ switch(cores){
         console.log("Alien"); // Don't use BREAK!
 
 }
+
+/**
+ * Basics Conception of Class (old and new way)
+ */
+
+let cell = function(){
+
+    this.color = "Black";
+
+    this.turnON = function()
+    {
+
+        console.log("Call");
+        return "calling";
+
+    }
+
+}
+
+let object = new cell();
+
+console.log(object.turnON());
+
+class cellPhone {
+
+    constructor(){
+
+        this.color2 = "White";
+
+    }
+
+}
+
+let object2 = new cellPhone();
+
+console.log(object2);
