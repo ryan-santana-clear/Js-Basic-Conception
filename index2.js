198f29e4d59f61aa485b3b86835d989246b746a4
Ryan Santana <ryan.santana.clear@gmail.com>
2023-02-14 11:03:47 -0300

    Practicing structure Switch/case

---
+++ index2.js
@@ -0,0 +1,28 @@
+/**
+ * Structures Condition Switch
+ */
+
+let cor = "Purple";
+
+switch(cor){
+
+    case "Blue":
+        console.log("Sky");
+        break;
+
+    case "Green":
+        console.log("Earth");
+        break;
+
+    case "Yellow":
+        console.log("Sun");
+        break;
+
+    case "Purple":
+        console.log("EggPlant");
+        break;
+
+    default:
+        console.log("Alien"); // Don't use BREAK!
+
+}
